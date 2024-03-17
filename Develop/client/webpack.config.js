ca59121f82f2cc6e85a2ff90d0c7478a09944f7b
Samantha Allen <samanthaamiraallen@gmail.com>
2024-03-17 10:09:44 -0300

    added inject manifest to webpack config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -22,6 +22,11 @@ module.exports = () => {
         template: './index.html',
         title: 'Text Editor'
       }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
 
     module: {
